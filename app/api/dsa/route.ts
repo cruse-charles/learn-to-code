f9ff5f0a78b6efd7fe68add1d14c6dc6145a1b26
Charles Cruse <charle9@vt.edu>
2024-12-10 02:02:26 +0800

    created api route to call for getQuestion function

---
+++ app/api/dsa/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { getQuestion } from '@/lib/mongo/dsa';
+
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const title = searchParams.get('title');
+
+    try {
+        const question = await getQuestion(title as string);
+        console.log('question:', question);
+
+        if (!question) {
+            return NextResponse.json({ error: 'Question not found' }, { status: 404 });
+        }
+
+        return NextResponse.json(question, { status: 200 });
+    } catch (error) {
+        console.error('Error fetching question:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
