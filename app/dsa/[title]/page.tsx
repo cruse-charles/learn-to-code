2684187d1e945fd99637924d9c0c814263b0409a
Charles Cruse <charle9@vt.edu>
2024-12-11 17:17:40 +0800

    changed question page to server component

--- app/dsa/[title]/page.tsx
+++ app/dsa/[title]/page.tsx
@@ -1,30 +1,12 @@
-'use client'
-import { useParams } from 'next/navigation'
-import { useState, useEffect } from 'react'
-
-
-export default function page() {
-    const {title} = useParams()
-    const [question, setQuestion] = useState(null)
-
-    useEffect(() => {
-        const fetchQuestion = async () => {
-            try {
-                const reponse = await fetch(`http://localhost:3000/api/dsa?title=${title}`)
-                const data = await reponse.json()
-                setQuestion(data)
-            } catch (error) {
-                console.error('Error fetching question:', error)
-            }
-        }
-
-        fetchQuestion()
-    }, [])
+export default async function page({params}) {
+    const {title} = await params
+    const reponse = await fetch(`http://localhost:3000/api/dsa?title=${title}`)
+    const question = await reponse.json()
 
   return (
     <>
-        <div>{title}</div>
-        <div>{question?.title}</div>
+        <div>Parms title: {title}</div>
+        <div>Fetching question: {question.title}</div>
     </>
   )
 }
