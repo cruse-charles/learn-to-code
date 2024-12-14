355002cf72796e84323e851c695ee603a20b4528
Charles Cruse <charle9@vt.edu>
2024-12-14 11:37:28 +0800

    fixed styling issue with header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,15 +6,15 @@ interface HeaderProps {
 export default function Header({title, subtitle}: HeaderProps) {
   return (
     <div className="flex justify-between items-center mb-12">
-    <div>
-      {/* Title and subtitle */}
-      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-indigo-600">
-        {title}
-      </h1>
-      <p className="mt-3 text-lg text-slate-600">
-        {subtitle}
-      </p>
+        <div>
+            {/* Title and subtitle */}
+            <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-indigo-600 leading-tight">
+                {title}
+            </h1>
+            <p className="mt-3 text-lg text-slate-600">
+                {subtitle}
+            </p>
+        </div>
     </div>
-  </div>
   )
 }
