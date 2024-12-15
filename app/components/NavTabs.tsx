b1997cf77dcad414e9c2b75559906ffdc40353f3
Charles Cruse <charle9@vt.edu>
2024-12-15 12:32:49 +0800

    added navtabs component

---
+++ app/components/NavTabs.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function NavTabs() {
+    const pathname = usePathname()
+
+    const navItems = [
+        {name: "Home", href: "/"},
+        {name: "Flashcards", href: "/flashcards"},
+        {name: "DSA", href: "/dsa"},
+        {name: "OOP", href: "/oop"}
+    ]
+
+    return (
+    <nav className="container border-b border-gray-200">
+      <div className="flex h-12 items-center space-x-4 text-sm font-medium">
+        {navItems.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={`
+              "transition-colors hover:text-gray-800",
+              ${pathname === item.href ? "border-b-2 border-violet-600 text-gray-900" : "text-gray-600"}
+            `}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  )
+}
