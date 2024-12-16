38d2b914632ecee7b7485bc0d1dd162bdef752de
Charles Cruse <charle9@vt.edu>
2024-12-16 17:09:16 +0800

    added comments to frontend component

--- app/dsa/LeftPane.tsx
+++ app/dsa/LeftPane.tsx
@@ -1,5 +1,3 @@
-import SubHeader from 'app/components/headers/SubHeader'
-
 interface LeftPaneProps {
   title: string;
   description: string;
@@ -11,17 +9,27 @@ interface LeftPaneProps {
 }
 
 function LeftPane({title, description, examples}: LeftPaneProps) {
-  console.log('examples', examples)
 
   return (
+    // background gradient and padding
     <div className='min-h-screen bg-gradient-to-b from-slate-100 to-violet-100 py-8 px-4 sm:px-4 lg:px-8'>
+      
+      {/* Card wrapper */}
       <div className='max-w-6xl mx-auto'>
+
+        {/* Card container for question information */}
         <div className='bg-white rounded-lg shadow-md p-6 border border-violet-100 mb-6'>
+          
+          {/* Title and Placeholder for tabs */}
           <h1 className="text-2xl font-bold text-slate-800">{title}</h1>
           <div className='mb-6'>Description/Solution tabs</div>
+          
+          {/* Description of problem */}
           <div className='prose max-w-none'>
             <p className="text-slate-700 mb-6">{description}</p>
           </div>
+          
+          {/* Examples section, iterate through example information */}
           <h3 className="text-lg font-semibold text-slate-800 mb-2">Examples:</h3>
           {examples.input.map((inputExample, index) => (
             <div className='mb-4 bg-slate-50 p-4 rounded-md'>
