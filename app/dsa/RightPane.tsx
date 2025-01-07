6e1263db15a6262dea85f7510838627bc0fa348f
Charles Cruse <charle9@vt.edu>
2025-01-07 16:53:48 +0800

    rendered output of test cases

--- app/dsa/RightPane.tsx
+++ app/dsa/RightPane.tsx
@@ -26,6 +26,7 @@ function RightPane({starterCode, testCases, descriptionExamples}: RightPaneProps
   // track user's code in the editor
   const [userCode, setUserCode] = useState(starterCode)
   const [selectedTestCase, setSelectedTestCase] = useState(0)
+  const [solutionArray, setSolutionArray] = useState([])
   
   const handleChange = (inputCode: string) => {
     setUserCode(inputCode)
@@ -37,7 +38,8 @@ function RightPane({starterCode, testCases, descriptionExamples}: RightPaneProps
     const userFunction = new Function(`return ${userCode}`)()
 
     // Call the function with test cases
-    const solutionArray = submitSolution(userFunction, testCases)
+    // const solutionArray = submitSolution(userFunction, testCases)
+    setSolutionArray(submitSolution(userFunction, testCases))
     console.log(solutionArray)
     // TODO: Display the results
   }
@@ -65,23 +67,35 @@ function RightPane({starterCode, testCases, descriptionExamples}: RightPaneProps
           <button onClick={handleSubmit} className='bg-violet-500 text-white px-4 py-2 rounded-md mt-4'>Run Code</button>
         </div>
     </div>
-    <div className="bg-white rounded-lg shadow-md border border-violet-100 overflow-hidden">
-      <div className="bg-slate-100 px-4 py-2 border-b border-slate-200">
+    <div className="bg-white rounded-lg overflow-hidden">
+      <div className="bg-slate-100 px-4 py-2 border-slate-200">
           <h3 className="font-medium">Test Results</h3>
       </div>
-      <div className='flex items-center'>
+      <div className="flex p-4 h-[calc(100%-56px)] overflow-auto">
         {descriptionExamples.map((_, index) => (
-          <div>
-            <div onClick={()=>handleExampleClick(index)}>Test Case {index + 1}</div>
+          <div
+            key={index}
+            onClick={() => handleExampleClick(index)}
+            className={`
+              px-4 py-2 cursor-pointer text-center transition
+              ${selectedTestCase === index ? 'bg-white font-semibold border-b-2 border-violet-500' : 'bg-slate-100'}
+              flex-1
+            `}
+          >
+            Test Case {index + 1}
           </div>
         ))}
       </div>
-      <div>
-        <div>Input: {descriptionExamples[selectedTestCase]?.input}</div>
-        <div>Output: {descriptionExamples[selectedTestCase]?.output}</div>
+      <div className="flex items-center justify-between mb-3">
+        <span className="font-medium">Test Case {selectedTestCase + 1}</span>
+        <div className={`p-4 rounded-md border ${
+                        solutionArray[selectedTestCase]?.isCorrect ? "border-green-200 bg-green-50" : "border-red-200 bg-red-50"
+                      }`}>
+          <div>Input: {descriptionExamples[selectedTestCase]?.input}</div>
+          <div>Output: {descriptionExamples[selectedTestCase]?.output}</div>
+        </div>
       </div>
     </div>
-
   </SplitPane>
   )
 }
